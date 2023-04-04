e1252d442aca2467725101e7c34c83cae5b6a36c
9652aman8956 <vektorfayt@gmail.com>
2023-04-04 16:08:43 +0600

    first commit

---
+++ less19.py
@@ -0,0 +1,75 @@
+# cars = dict({'name' : 'bmw','age':1234})
+# print(cars)
+
+
+# dictionary_1 = {'a':300,'b':400}
+# dictionary_2 = {'c':500,'d':600}
+# dict_3 = {}
+
+
+# for i in (dictionary_1,dictionary_2): dict_3.update(i)
+# print(dicty_3)
+
+
+# for i in zip(dictionary_1.items() , dictionary_2.items()):
+#      dict_3.update(i)   
+# print(dict_3)
+
+
+# my_dictionary = {'data1':5,'data2':6,'data3':7,'data4':8}
+# result = 1
+
+# for i in my_dictionary:
+#      result  *= my_dictionary[i]
+# print(result)
+
+
+                                                                      # ДЗ
+
+
+# my_dict = {i : i ** 3 for i in range(1, 11)}
+# print(my_dict)
+
+
+# d = dict()
+# for i in range(1,11):
+#     d[i]=i**3
+# print(d)
+
+
+
+
+
+# color= ['red', 'green', 'blue']
+# code = ['#FF0000','#008000', '#0000FF']
+# new_dict = dict(zip(color , code))
+# new_dict['white'] = '#fffff'
+# print(new_dict)
+
+# color_dictionary = dict(zip(color, code))
+# print(color_dictionary)
+
+# for k, v in zip(color, code):
+#     dict3[k] = v
+
+# print(dict3)
+
+
+
+
+
+
+# str1 = 'pythonist'
+# lst = []
+# for i in str1:
+#     count = str1.count(i)
+#     lst.append(count)
+# a = dict(list(zip(str1,lst)))
+# print(a)
+
+# my_dict = {k: str1.count(k) for k in str1}
+# print(my_dict)
+
+
+
+
